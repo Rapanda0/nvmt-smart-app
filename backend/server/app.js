a502a3142be5bb5ca8888b4aa51ce9b2e06a9a25
Rapanda0 <ryel.rapada07@gmail.com>
2024-03-30 22:54:10 -0600

    node modules

--- backend/server/app.js
+++ backend/server/app.js
@@ -51,6 +51,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
 
+
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
