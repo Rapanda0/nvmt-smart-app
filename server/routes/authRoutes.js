de24eee75ad2c5eea1123556f992667920c1fc29
Rapanda0 <ryel.rapada07@gmail.com>
2024-04-05 21:11:51 -0600

    removing bcrypt necessary files

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const pool = require('../db');
 const router = express.Router();
 const saltRounds = 10;
@@ -7,31 +7,31 @@ const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
 
 // Registration route with validation
-router.post('/register', [
-  body('username', 'Username is required').not().isEmpty(),
-  body('username').isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
-  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
+// router.post('/register', [
+//   body('username', 'Username is required').not().isEmpty(),
+//   body('username').isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
+//   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+// ], async (req, res) => {
+//   const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     return res.status(400).json({ errors: errors.array() });
+//   }
   
-  try {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+//   try {
+//     const { username, password } = req.body;
+//     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    const newUser = await pool.query(
-      'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *',
-      [username, hashedPassword]
-    );
+//     const newUser = await pool.query(
+//       'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *',
+//       [username, hashedPassword]
+//     );
 
-    res.status(201).json(newUser.rows[0]);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send('Server error');
-  }
-});
+//     res.status(201).json(newUser.rows[0]);
+//   } catch (error) {
+//     console.error(error.message);
+//     res.status(500).send('Server error');
+//   }
+// });
 
 // Login route with validation
 router.post('/login', [
